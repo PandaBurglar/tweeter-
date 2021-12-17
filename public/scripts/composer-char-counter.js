0fdb165dbb6b9c24d29c9d4647b2d76a10a39609
PandaBurglar <zane.bokhari@gmail.com>
2021-12-17 15:42:05 -0500

    makes a live character coount of text in the textarea. If the count is below zero, it will turn the counter red and show -character count.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  console.log('The document is ready!');
+
+  //makes character counter a live count of text chars in textarea
+  $('#tweet-text').on('input', function(e) {
+    let charCount = $(this).val().length;
+    let remainingChars = 140 - charCount;
+
+    //dom traversal targeting of .counter value
+    let counter = $(this).parent().next('div').children('.counter');
+    counter.text(remainingChars);
+
+    if (remainingChars < 0) {
+      counter.addClass('redCounter');
+    } else {
+      counter.removeClass('redCounter');
+    }
+  });
+});
