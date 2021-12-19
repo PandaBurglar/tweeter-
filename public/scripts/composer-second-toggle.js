d0ed939ce287104428cd47f4e5e7e9ecea33e897
PandaBurglar <zane.bokhari@gmail.com>
2021-12-19 18:14:02 -0500

    cleaning up code, the functionality for the second toggle button is moved into this file.

---
+++ public/scripts/composer-second-toggle.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  console.log('doc is ready')
+
+  $('form.submitATweet').on('submit', submitTweetPost)
+
+  $('.writeATweet').on('click', function() {
+    $('.new-tweet').slideToggle(200);
+  });
+
+});
